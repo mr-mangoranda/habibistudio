82f763a0ffe5fbe25069cbf3db11b0f940293e09
mr-mangoranda <habibmangoranda15@gmail.com>
2026-02-21 11:37:59 +0800

    Layouts and Components

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+        <div>@2026 Habibi. All rights reserved.</div>
+        <div></div>
+    </div>
+  )
+}
+
+export default Footer
