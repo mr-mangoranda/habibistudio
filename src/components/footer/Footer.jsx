625e9bc2f9e7ff06d47859a0589fc2ba84350a48
mr-mangoranda <habibmangoranda15@gmail.com>
2026-02-24 05:44:34 +0800

    Global and Module CSS

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import styles from './footer.module.css'
 
 function Footer() {
+
   return (
-    <div>
-        <div>@2026 Habibi. All rights reserved.</div>
+    <div className={styles.container}>
+        <div className={styles}>@2026 Habibi. All rights reserved.</div>
         <div></div>
     </div>
   )
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,7 @@
+"use client"
 import React from 'react'
 import Link from 'next/link'
+import styles from './navbar.module.css'
 
 const links = [
     {
@@ -36,14 +38,17 @@ const links = [
 
 function Navbar() {
   return (
-    <div>
-        <Link href="/">Habibi</Link>
-        <div>
+    <div className={styles.container}>
+        <Link href="/" className={styles.logo}>Mangoranda</Link>
+        <div className={styles.links}> 
             {links.map((link) => (
-                <Link key={link.id} href={link.url}>
+                <Link key={link.id} href={link.url} className={styles.link}>
                     {link.title}
                 </Link>
             ))}
+            <button 
+                className={styles.logout}
+                onClick={()=>{console.log("logged out");}}>Logout</button>
         </div>
     </div>
   )
