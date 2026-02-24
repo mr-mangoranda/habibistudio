6f985c50a904428765a383438138c9a497209095
mr-mangoranda <habibmangoranda15@gmail.com>
2026-02-24 09:23:37 +0800

    About / Contact Section

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
-import styles from './footer.module.css'
-
+import React from 'react';
+import styles from './footer.module.css';
+import Image from 'next/image';
 function Footer() {
 
   return (
     <div className={styles.container}>
-        <div className={styles}>@2026 Habibi. All rights reserved.</div>
-        <div></div>
+        <div>@2026 Habibi. All rights reserved.</div>
+        <div className={styles.social}>
+          <Image src="/1.png" width={15} height={15} className={styles.icon} alt="Mangoranda Facebook Account"/>
+          <Image src="/2.png" width={15} height={15} className={styles.icon} alt="Mangoranda Dev"/>
+          <Image src="/3.png" width={15} height={15} className={styles.icon} alt="Mangoranda Dev"/>
+          <Image src="/4.png" width={15} height={15} className={styles.icon} alt="Mangoranda Dev"/>
+        </div>
     </div>
   )
 }
