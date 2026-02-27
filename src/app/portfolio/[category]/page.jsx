ebfc7d619b1651e12f6182e43d36b6fa155b4360
mr-mangoranda <habibmangoranda15@gmail.com>
2026-02-27 11:13:09 +0800

    Portfolio Section

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,8 +1,44 @@
 import React from 'react'
+import styles from './page.module.css'
+import Button from '@/components/Button/Button'
+import Image from 'next/image'
 
-function Category() {
+const Category = ({params}) => {
+  console.log(params)
   return (
-    <div>Category</div>
+    <div className={styles.container}>
+      <h1 className={styles.catTitle}>{params.category}</h1>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+          <Button text="See More" url="#" />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image 
+            className={styles.img}
+            fill={true}
+            src=""
+            alt=''
+          ></Image>
+        </div>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+          <Button text="See More" url="#" />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image 
+            className={styles.img}
+            fill={true}
+            src=""
+            alt=''
+          ></Image>
+        </div>
+      </div>
+    </div>
   )
 }
 
